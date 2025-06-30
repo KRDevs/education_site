f2ea124aa753932513276c08c25e1b6260e97d82
KRDevs <raximovkamron3@gmail.com>
2025-06-30 22:51:50 +0500

    ok

--- subject/admin.py
+++ subject/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from subject.models import Lecture, Test, TestOption, Practice, Laboratory
+from subject.models import Lecture, Test, TestOption, Practice, Laboratory, Videos
 
 admin.site.register(Lecture)
 admin.site.register(Practice)
 admin.site.register(Laboratory)
 admin.site.register(Test)
 admin.site.register(TestOption)
+admin.site.register(Videos)
